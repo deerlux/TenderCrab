887b9c7c61300cc6cb8f5e8bac31f8d1625bb3a3
deerlux <deerlux@163.com>
2020-03-17 22:47:46 +0800

    改为下一页，并添加页码参数

--- TenderCrab/spiders/TenderSD.py
+++ TenderCrab/spiders/TenderSD.py
@@ -12,6 +12,10 @@ class TendersdSpider(scrapy.Spider):
     start_urls = ['http://www.ccgp-shandong.gov.cn/sdgp2017/site/channelall.jsp?colcode=0302',
         'http://www.ccgp-shandong.gov.cn/sdgp2017/site/channelall.jsp?colcode=0304']
 
+    def __init__(self, pages=None, *args, **kwargs):
+        super(TendersdSpider, self).__init__(*args, **kwargs)
+        self.pages = pages
+
     # rules = (
     #     Rule(LinkExtractor(allow=r'&id=\d+'), callback='parse_item', follow=True),
     # )
@@ -19,12 +23,16 @@ class TendersdSpider(scrapy.Spider):
     def parse(self, response):
         base_url = response.url.split('?')[0]
         colcode = re.findall(r'colcode=(\d+)', response.url)[0]
+        curpage = 1
+        temp = re.findall(r'curpage=(\d+)', response.url)
+        if temp:
+            curpage = temp[0]
 
         self.logger.debug(f'The URL of parse() is: {response.url}')
         # ---- 首先将有链接的标讯提取出来，然后送给另外一个回调函数去解析
         sels = response.xpath(r'//td[@class="Font9"]')
         # 只在前面的td中找相关的链接
-        sels.pop(-1)
+        pageNumSel = sels.pop(-1)
         self.logger.debug(f'Find {len(sels)} urls.....')
         for sel in sels:
             temp = sel.xpath(r'./text()').extract()
@@ -54,22 +62,36 @@ class TendersdSpider(scrapy.Spider):
                 )
             yield request
 
-        # ---- 然后将所有的页进行解析返回
-        sels = response.xpath(r'//td[@class="Font9"]/a')
-        # 最后一个指出尾页的数字 javascript:query(1756)
-        endPageScr = sels[-1].xpath(r'./@href').extract()[0]
-        endPage = int(re.findall(r'\d+', endPageScr)[0])
-        # 遍历每一页
-        for k in range(endPage):
-            # 第一页跳过
-            if k == 0:
-                continue
-            # if k == 2:
-            #     break
-            url = f'{base_url}?colcode={colcode}&curpage={k + 1}'
-            request = scrapy.Request(url, callback=self.parse)
-            self.logger.info(f'The number of pages is: {k + 1}')
-            yield request
+        if (not self.pages) or (curpage < self.pages):
+            # 解析下一页
+            sels = pageNumSel.xpath(r'./a')
+            url = f'{base_url}?colcode={colcode}'
+            for sel in sels:
+                if sel.xpath(r'./text()')[0] == "下一页":
+                    href = sel.xpath(r'./@href').extract()[0]
+                    nextPage = re.findall(r'\d+', href)[0]
+                    url = f'{url}&curpage={nextPage}'
+                    self.logger.debug(f'The number of page is: {nextPage}')
+                    yield scrapy.Request(url, self.parse)
+
+        # # 如果是curpage = 2进来的，则不再解析每一页
+        # if curpage == 1:
+        #     # ---- 然后将所有的页进行解析返回
+        #     sels = response.xpath(r'//td[@class="Font9"]/a')
+        #     # 最后一个指出尾页的数字 javascript:query(1756)
+        #     endPageScr = sels[-1].xpath(r'./@href').extract()[0]
+        #     endPage = int(re.findall(r'\d+', endPageScr)[0])
+        #     # 遍历每一页
+        #     for k in range(2, endPage + 1):
+        #         # 第一页跳过
+        #         if k == 0:
+        #             continue
+        #         # if k == 2:
+        #         #     break
+        #         url = f'{base_url}?colcode={colcode}&curpage={k}'
+        #         request = scrapy.Request(url, callback=self.parse)
+        #         self.logger.info(f'The number of pages is: {k}')
+        #         yield request
 
     def parse_item(self, response, publishDate):
         # self.logger.debug(f'Parsing URL: {response.url}')
