a3b593fe848e3621df63c165e9d656c6dbec766a
deerlux <deerlux@163.com>
2022-05-04 22:20:02 +0800

    基本能够定期抓取数据，无错误出现。只抓取body部分，无解析

--- TenderCrab/spiders/TenderSD.py
+++ TenderCrab/spiders/TenderSD.py
@@ -4,18 +4,15 @@ from TenderCrab.items import TendercrabItem
 from dateutil.parser import parse, ParserError
 from scrapy.http import Response
 import re
-from sqlalchemy.orm import sessionmaker
-import sqlalchemy as sa
-import TenderCrab.settings as settings
-from TenderCrab.DataModels import TenderItem, get_url_hashset
+
+from TenderCrab.DataModels import get_url_hashset
 
 
 class TendersdSpider(scrapy.Spider):
     name = 'TenderSD'
     url_hashset = set()
-    engine = sa.create_engine(settings.DBURL)
-    Session = sessionmaker(bind=engine)    
-    
+    # allowed_domain = ['ccgp-shandong.gov.cn']
+
     def __init__(self, pages=None, *args, **kwargs):
         super(TendersdSpider, self).__init__(*args, **kwargs)
         self.start_urls = ['http://ccgp-shandong.gov.cn/sdgp2017/site/listnew.jsp?grade=province&colcode=0302',
@@ -26,13 +23,11 @@ class TendersdSpider(scrapy.Spider):
             self.pages = None
 
         # 以下对数据库的内容进行更新
-        if self.url_hashset:
-            return        
-        with self.Session() as session:
+        if not self.url_hashset:
             self.url_hashset = get_url_hashset()
- 
+            self.logger.info("The url_hashset is updated.")
 
-    def parse(self, response: Response):        
+    def parse(self, response: Response):
         self.logger.debug(f'Current URL: {response.url}')
         # 抓取相关的标讯内容链接
         links = response.css('span.title span a')
@@ -41,32 +36,25 @@ class TendersdSpider(scrapy.Spider):
         if len(links) == 0:
             yield
         if len(links) != len(pubDates):
+            self.logger.debug('Links length and pubDates are not equal!')
             yield
                 
         for link, pubDate in zip(links, pubDates):
             try:
-                url = link.css('::attr("href")').extract()[0]
-                title = link.css('::attr("title")').extract()[0]
-                pubDate = pubDate.css('::text').extract()[0]
+                url = link.attrib['href']
+                title = link.attrib['title']
+                pubDate = pubDate.css('::text').get()
                 
                 url = response.urljoin(url)
-                # 如果已经抓取过了，则更新title
-                if hash(url) in self.url_hashset:
-                    item = TendercrabItem()
-                    item['url'] = url
-                    item['title'] = title
-                    try:
-                        item['publishDate'] = parse(pubDate)
-                    except ParserError:
-                        item['publishDate'] = None
-                        self.logger.info(f'Error to parse publishDate: {pubDate}')                    
-                    yield item
-                else:
-                    self.logger.debug(f'Yield the tender pages: {url}')
+                # 如果这个URL没的被抓取过
+                if hash(url) not in self.url_hashset:
+                    self.logger.info(f'Yield the tender pages: {url}')
                     yield response.follow(url, callback=self.parse_item, 
                             cb_kwargs={'title': title, 
                             'publishDate': pubDate
                             })
+                else:
+                    self.logger.debug(f'Skipping url: {url}')
             except IndexError:
                 self.logger.debug('Failed to extract url from span.')
 
@@ -86,13 +74,13 @@ class TendersdSpider(scrapy.Spider):
 
         # 首先将所有的页面都排到队列里
         if curpage == 1:
-            temp = response.css('#totalnum::text').getall()
+            temp = response.css('#totalnum::text').get()
             if temp:
-                totalpage = int(temp[0])
+                totalpage = int(temp)
             else:
                 totalpage = 1
             crawl_pages = self.pages if self.pages else totalpage
-            if crawl_pages ==1:
+            if crawl_pages == 1:
                 yield
 
             for i in range(curpage + 1, crawl_pages + 1):
@@ -101,9 +89,8 @@ class TendersdSpider(scrapy.Spider):
                 self.logger.debug(f'Yield URL: {url}')
                 yield response.follow(url, self.parse)
 
-
     def parse_item(self, response, title, publishDate):
-        # self.logger.debug(f'Parsing URL: {response.url}')
+        self.logger.info(f'Parsing URL: {response.url}')
         item = TendercrabItem()
         item['url'] = response.url
         item['title'] = title
@@ -118,5 +105,3 @@ class TendersdSpider(scrapy.Spider):
         item['price'] = None
         item['seller'] = ''
         yield item
-        
-
